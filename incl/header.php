ed76ada6a29a526ebfc8943860a285aac74ae816
Cheryl Velez <onegrumpybunny@gmail.com>
2019-01-24 12:59:32 -0500

    Create header.php

---
+++ incl/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html class=" js csstransforms csstransforms3d csstransitions" lang="en">
+<head> 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta description="Annual Report 2018">
+    <meta name="robots" content="index, follow">
+    <meta name="description" content="At RTI International, we look continually toward the future, exploring how research, knowledge, and scientific discovery can make the world a place where people thrive. In 2018, we remained steadfast in our mission to do work that improves lives—work with impact that radiates outward, from individuals and families to communities, cities, and even nations. We strive to shift the boundaries between what is and what could be, knowing that our persistence of vision can shape a brighter future for all.">
+    <title>Shaping the Future | RTI International 2018 Annual Report</title>
+    <link rel="shortcut icon" href="https://www.rti.org/sites/default/files/favicon.png" type="image/png">
+    <meta property="og:url" content="https://annualreport.rti.org/2018/">
+    <meta property="og:site_name" content="RTI International 2018 Annual Report">
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="Shaping the Future | RTI International 2018 Annual Report">
+    <meta property="og:description" content="At RTI International, we look continually toward the future, exploring how research, knowledge, and scientific discovery can make the world a place where people thrive. In 2018, we remained steadfast in our mission to do work that improves lives—work with impact that radiates outward, from individuals and families to communities, cities, and even nations. We strive to shift the boundaries between what is and what could be, knowing that our persistence of vision can shape a brighter future for all.">
+    <meta property="og:image" content="https://annualreport.rti.org/2018/img/og.jpg">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@RTI_Intl">
+    <meta name="twitter:creator" content="@RTI_Intl">
+    <meta name="twitter:title" content="Living the Mission | RTI International 2018 Annual Report">
+    <meta name="twitter:description" content="At RTI International, we look continually toward the future, exploring how research, knowledge, and scientific discovery can make the world a place where people thrive. In 2018, we remained steadfast in our mission to do work that improves lives—work with impact that radiates outward, from individuals and families to communities, cities, and even nations. We strive to shift the boundaries between what is and what could be, knowing that our persistence of vision can shape a brighter future for all.">
+    <meta name="twitter:image" content="https://annualreport.rti.org/2018/img/og.jpg">
+    
+    <link rel="stylesheet" href="https://use.typekit.net/vxh7raq.css">
+  
+
+
+    <link rel='stylesheet' href='<?php $globals["CSS_DIR"]?>'/normalize.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='<?php $globals["CSS_DIR"]?>'/grid.css' type='text/css' media='all' />
+    <script type='text/javascript' src='<?php $globals["JS_DIR"]?>'/jquery.min.js'></script>
+
+</head>
+<body>
+    
