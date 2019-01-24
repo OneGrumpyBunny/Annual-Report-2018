950ea2ac1e72d3cf8c607d535d57cd48b8716e85
Cheryl Velez <onegrumpybunny@gmail.com>
2019-01-24 12:56:26 -0500

    setting up PHP Environ

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/*  PROD  */
+//$globals["BASE_URL"] = "http://18.207.215.79/index.php";
+
+/*  DEV  */
+$globals["BASE_URL"] = "http://18.207.215.79/dev/index.php";
+
+$globals["INCL_DIR"] = "incl/";
+$globals["IMG_DIR"] = "img/";
+$globals["JS_DIR"] = "js/";
+$globals["CSS_DIR"] = "css/";
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php session_start();
+
+/* turn on error reporting */
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+/* end error reporting */
+
+include 'config.php';
+
+include $globals["INCL_DIR"].'header.php';
+include $globals["INCL_DIR"].'content.php';
+include $globals["INCL_DIR"].'footer.php';
+	
+?>
