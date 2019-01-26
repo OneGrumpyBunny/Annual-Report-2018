ac7f001d2c684d7d4185de5a21dd12e2e93a61e7
Cheryl Velez <onegrumpybunny@gmail.com>
2019-01-26 08:44:39 -0500

    Adding hover effects to navbar

--- js/ar.js
+++ js/ar.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-    $(".about").mouseenter(function() {
+    $(".navBox").mouseenter(function() {
         //console.log("mouseenter");
-        $(".about .insideBox2").css("background","rgba(48, 96, 149, 1);");
-        $(".about .insideBox3 p").css("opacity","1");
+        $(this + " .insideBox2").css("background","rgba(48, 96, 149, 1)");
+        $(this + " .insideBox3 p").css("opacity","1");
+        $(this + " .insideBox3").css("background","rgba(50, 59, 69, .8)");
     });
+    
 
 });
