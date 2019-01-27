0370f1ca81b993b7bcbe5248fa30d017c20a7f57
Cheryl Velez <onegrumpybunny@gmail.com>
2019-01-27 12:02:23 -0500

    header

--- incl/header.php
+++ incl/header.php
@@ -8,13 +8,20 @@
     <meta description="Annual Report 2018">
     
     <link rel="stylesheet" href="https://use.typekit.net/vxh7raq.css">
-    <link rel='stylesheet' href='/dev/css/normalize.css' />
-    <link rel='stylesheet' href='/dev/css/flex.css' />
-    <link rel='stylesheet' href='/dev/css/styles.css' />
-	<link rel='stylesheet' href='/dev/css/tablet.css' />
-	<script type='text/javascript' src='/dev/js/jquery.min.js'></script>
-	<script type='text/javascript' src='/dev/js/stretchtext.js'></script>
-    <script type='text/javascript' src='/dev/js/ar.js'></script>
+
+    <link rel="stylesheet" href="/dev/css/normalize.css">
+
+    <link rel="stylesheet" href="/dev/css/flex.css">
+
+    <link rel="stylesheet" href="/dev/css/styles.css">
+
+	<link rel="stylesheet" href="/dev/css/tablet.css">
+
+	<script type="text/javascript" src="/dev/js/jquery.min.js"></script>
+
+	<script type="text/javascript" src="/dev/js/stretchtext.js"></script>
+
+    <script type="text/javascript" src="/dev/js/ar.js"></script>
     
 </head>
 <body>
