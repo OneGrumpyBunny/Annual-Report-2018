1e89dcdd72dce771cb0c9252ea23e9b15238e938
Cheryl Velez <onegrumpybunny@gmail.com>
2019-01-27 11:54:56 -0500

    troubleshooting php includes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include $globals["BASE_URL"].'config.php';
 
-include $globals["INCL_DIR"].'header.php';
+include $globals["INCL_DIR"].'header.html';
 include 'content.php';
 include $globals["INCL_DIR"].'nav.php';
 include $globals["INCL_DIR"].'footer.php';
