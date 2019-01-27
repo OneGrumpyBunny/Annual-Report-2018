49ff6d1f9ac646eaaf266a4ad7bb4005d8083615
Cheryl Velez <onegrumpybunny@gmail.com>
2019-01-27 11:43:11 -0500

    Update header.php

--- incl/header.php
+++ incl/header.php
@@ -8,14 +8,22 @@
     <meta description="Annual Report 2018">
     
     <link rel="stylesheet" href="https://use.typekit.net/vxh7raq.css">
-    <link rel='stylesheet' href='<?php $globals["CSS_DIR"]?>/normalize.css' />
-    <link rel='stylesheet' href='<?php $globals["CSS_DIR"]?>/flex.css' />
-    <link rel='stylesheet' href='<?php $globals["CSS_DIR"]?>/styles.css' />
-	<link rel='stylesheet' href='<?php $globals["CSS_DIR"]?>/tablet.css' />
-	<script type='text/javascript' src='<?php $globals["JS_DIR"]?>/jquery.min.js'></script>
-	<script type='text/javascript' src='<?php $globals["JS_DIR"]?>/stretchtext.js'></script>
-	<script type='text/javascript' src='<?php $globals["JS_DIR"]?>/ar.js'></script>
-
+    <link rel='stylesheet' href='/dev/css/normalize.css' />
+    <link rel='stylesheet' href='/dev/css/flex.css' />
+    <link rel='stylesheet' href='/dev/css/styles.css' />
+	<link rel='stylesheet' href='/dev/css/tablet.css' />
+	<script type='text/javascript' src='/dev/js/jquery.min.js'></script>
+	<script type='text/javascript' src='/dev/js/stretchtext.js'></script>
+    <script type='text/javascript' src='/dev/js/ar.js'></script>
+    <?php
+    /*echo "<link rel='stylesheet' href='".$globals["CSS_DIR"]."/normalize.css' />";
+    echo "<link rel='stylesheet' href='".$globals["CSS_DIR"]."/flex.css' />";
+    echo "<link rel='stylesheet' href='".$globals["CSS_DIR"]."/styles.css' />";
+	echo "<link rel='stylesheet' href='".$globals["CSS_DIR"]."/tablet.css' />";
+	echo "<script type='text/javascript' src='".$globals["JS_DIR"]."/jquery.min.js'></script>";
+	echo "<script type='text/javascript' src='".$globals["JS_DIR"]."/stretchtext.js'></script>";
+	echo "<script type='text/javascript' src='".$globals["JS_DIR"]."/ar.js'></script>";*/
+    ?>
 </head>
 <body>
     
