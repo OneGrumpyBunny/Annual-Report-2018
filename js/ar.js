ea231a3e425bf8d1e8f2537bc784eceac856fc9a
Cheryl Velez <onegrumpybunny@gmail.com>
2019-01-28 13:22:42 -0500

    styles

--- js/ar.js
+++ js/ar.js
@@ -27,58 +27,59 @@ var backOn = 0;
 });
 
 $(document).ready(function() {
+
     /* mouseenter actions - these will be consolidated into one function */
     $(".about").mouseenter(function() {
         $(".about .insideBox2").css("background","rgba(15, 57, 160, 1)");
         //$(".about .insideBox3 p").css("opacity","1");
-        $(".about .insideBox3").css("background","rgba(50, 59, 69, .5)");
+        $(".about .insideBox3").css("background","rgba(15, 57, 160, .5)");
     });
     $(".health").mouseenter(function() {
         $(".health .insideBox2").css("background","rgba(49, 96, 148, 1)");
         //$(".health .insideBox3 p").css("opacity","1");
-        $(".health .insideBox3").css("background","rgba(42, 49, 57, .5)");
+        $(".health .insideBox3").css("background","rgba(49, 96, 148, .5)");
     });
     $(".safe").mouseenter(function() {
         $(".safe .insideBox2").css("background","rgba(120, 124, 60, 1)");
         //$(".safe .insideBox3 p").css("opacity","1");
-        $(".safe .insideBox3").css("background","rgba(63, 64, 56, .5)");
+        $(".safe .insideBox3").css("background","rgba(120, 124, 60, .5)");
     });
     $(".thrive").mouseenter(function() {
         $(".thrive .insideBox2").css("background","rgba(99, 140, 28, 1)");
         //$(".thrive .insideBox3 p").css("opacity","1");
-        $(".thrive .insideBox3").css("background","rgba(60, 68, 46, .5)");
+        $(".thrive .insideBox3").css("background","rgba(99, 140, 28, .5)");
     });
     $(".planet").mouseenter(function() {
         $(".planet .insideBox2").css("background","rgba(255, 198, 41, 1)");
         //$(".planet .insideBox3 p").css("opacity","1");
-        $(".planet .insideBox3").css("background","rgba(88, 77, 49, .5)");
+        $(".planet .insideBox3").css("background","rgba(255, 198, 41, .5)");
     });
     
     /* mouseleave actions - these will be consolidated into one function */
     $(".about").mouseleave(function() {
         $(".about .insideBox2").css("background","rgba(15, 57, 160, .8)");
         //$(".about .insideBox3 p").css("opacity",".9");
-        $(".about .insideBox3").css("background","rgba(50, 59, 69, .8)");
+        $(".about .insideBox3").css("background","rgba(15, 57, 160, .8)");
     });
     $(".health").mouseleave(function() {
         $(".health .insideBox2").css("background","rgba(49, 96, 148, .8)");
         //$(".health .insideBox3 p").css("opacity",".9");
-        $(".health .insideBox3").css("background","rgba(42, 49, 57, .8)");
+        $(".health .insideBox3").css("background","rgba(49, 96, 148, .8)");
     });
     $(".safe").mouseleave(function() {
         $(".safe .insideBox2").css("background","rgba(120, 124, 60, .8)");
         //$(".safe .insideBox3 p").css("opacity",".9");
-        $(".safe .insideBox3").css("background","rgba(63, 64, 56, .8)");
+        $(".safe .insideBox3").css("background","rgba(120, 124, 60, .8)");
     });
     $(".thrive").mouseleave(function() {
         $(".thrive .insideBox2").css("background","rgba(99, 140, 28, .8)");
         //$(".thrive .insideBox3 p").css("opacity",".9");
-        $(".thrive .insideBox3").css("background","rgba(60, 68, 46, .8)");
+        $(".thrive .insideBox3").css("background","rgba(99, 140, 28, .8)");
     });
     $(".planet").mouseleave(function() {
         $(".planet .insideBox2").css("background","rgba(255, 198, 41, .8)");
         //$(".planet .insideBox3 p").css("opacity",".9");
-        $(".planet .insideBox3").css("background","rgba(88, 77, 49, .8)");
+        $(".planet .insideBox3").css("background","rgba(255, 198, 41, .8)");
     });
 
 });
