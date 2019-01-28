7bd210fc129088a981b2c5bfb3e1a66d632a2e65
Cheryl Velez <onegrumpybunny@gmail.com>
2019-01-28 17:57:23 -0500

    enhancing health landing page

--- enhancing-health/content.php
+++ enhancing-health/content.php
@@ -1,6 +1,22 @@
 
 	<div class="column ten flex">
 		<div class="enhancing-health">
+			<div>
+				<h1>Enhancing Health</h1>
+				<p class="light">Improving human health is at the core of RTI’s mission to improve the human condition. Time is of the essence for the scientists and researchers tackling these issues, and we specialize in taking real-time results and applying those to our work to combat HIV, cancer, heart disease, and opioid misuse. We imagine a world free of disease and dedicate ourselves to the intervention, prevention, and treatment necessary to save lives.</p>
+			</div>
+			<div>
+				<p class="regular">Learn more about how we are enhancing individual and family health:</p>
+				<ul>
+					<li><a href="">Envisioning a World Without Tropical Diseases</a></li>
+					<li><a href="">Stemming Chronic Diseases with Product Taxes?</a></li>
+					<li><a href="">Speeding Treatment Times for Heart Attacks</a></li>
+					<li><a href="">Leveling the HIV Treatment Field</a></li>
+					<li><a href="">Treating HIV and Opioid Abuse Together in Tanzania</a></li>
+					<li><a href="">Focusing on Filipino Family Health</a></li>
+					<li><a href="">Holding Addiction Providers Accountable</a></li>
+				</ul>
+			</div>
 		</div>
 	</div>
 	
