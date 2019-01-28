d8ce35defc7571fcb2a86c0b36b9f7d04f3e2c6d
Cheryl Velez <onegrumpybunny@gmail.com>
2019-01-28 12:25:01 -0500

    Updates to images, js and css

--- js/ar.js
+++ js/ar.js
@@ -1,16 +1,31 @@
 $(window).on("load",function() {
-
+	if ($(".living-our-mission").is(":visible")) {
+		$("body").css("background","url('../living-our-mission-back.png')");
+	}
+	if ($(".enhancing-health").is(":visible")) {
+		$("body").css("background","url('../enhancing-health-back.png')");
+	}
+	if ($(".keeping-people-safe").is(":visible")) {
+		$("body").css("background","url('../keeping-people-safe-back.png')");
+	}
+	if ($(".thrive-and-prosper").is(":visible")) {
+		$("body").css("background","url('../thrive-and-prosper-back.png')");
+	}
+	if ($(".protecting-the-planet").is(":visible")) {
+		$("body").css("background","url('../protecting-the-planet-back.png')");
+	}
+	
 });
 
 $(document).ready(function() {
     /* mouseenter actions - these will be consolidated into one function */
     $(".about").mouseenter(function() {
-        $(".about .insideBox2").css("background","rgba(36, 136, 176, 1)");
+        $(".about .insideBox2").css("background","rgba(15, 57, 160, 1)");
         //$(".about .insideBox3 p").css("opacity","1");
         $(".about .insideBox3").css("background","rgba(50, 59, 69, .5)");
     });
     $(".health").mouseenter(function() {
-        $(".health .insideBox2").css("background","rgba(34, 106, 160, 1)");
+        $(".health .insideBox2").css("background","rgba(49, 96, 148, 1)");
         //$(".health .insideBox3 p").css("opacity","1");
         $(".health .insideBox3").css("background","rgba(42, 49, 57, .5)");
     });
@@ -32,12 +47,12 @@ $(document).ready(function() {
     
     /* mouseleave actions - these will be consolidated into one function */
     $(".about").mouseleave(function() {
-        $(".about .insideBox2").css("background","rgba(36, 136, 176, .8)");
+        $(".about .insideBox2").css("background","rgba(15, 57, 160, .8)");
         //$(".about .insideBox3 p").css("opacity",".9");
         $(".about .insideBox3").css("background","rgba(50, 59, 69, .8)");
     });
     $(".health").mouseleave(function() {
-        $(".health .insideBox2").css("background","rgba(34, 106, 160, .8)");
+        $(".health .insideBox2").css("background","rgba(49, 96, 148, .8)");
         //$(".health .insideBox3 p").css("opacity",".9");
         $(".health .insideBox3").css("background","rgba(42, 49, 57, .8)");
     });
