ec71041656a426b4e131a1fbb34b6a7d972bbfc8
Cheryl Velez <onegrumpybunny@gmail.com>
2019-01-28 09:01:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include $globals["BASE_URL"].'config.php';
 
-include $globals["INCL_DIR"].'header.html';
+include $globals["INCL_DIR"].'header.php';
 include 'content.php';
 include $globals["INCL_DIR"].'nav.php';
 include $globals["INCL_DIR"].'footer.php';
