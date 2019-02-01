d8334acbf9412e1770364cbde439b734854c0fc7
Gieck <cgieck@rti.org>
2019-02-01 11:53:38 -0500

    living our mission meta

--- living-our-mission/index.php
+++ living-our-mission/index.php
@@ -3,6 +3,7 @@ $REL_DIR = "../";
 include $REL_DIR."config.php";
 
 include $REL_DIR.$INCL_DIR."header.php";
+include $REL_DIR.$INCL_DIR."meta.php";
 include "content.php";
 include $REL_DIR.$INCL_DIR."nav.php";
 include $REL_DIR.$INCL_DIR."footer.php";
