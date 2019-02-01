a7d2eba73a56348e5045f94763924e3817ae6015
Gieck <cgieck@rti.org>
2019-02-01 11:09:20 -0500

    thrive

--- helping-regions-thrive-and-prosper/index.php
+++ helping-regions-thrive-and-prosper/index.php
@@ -3,7 +3,7 @@ $REL_DIR = "../";
 include $REL_DIR."config.php";
 
 include $REL_DIR.$INCL_DIR."header.php";
-include "meta.php";
+include $REL_DIR.$INCL_DIR."meta-thrive.php";
 include "content.php";
 include $REL_DIR.$INCL_DIR."nav.php";
 include $REL_DIR.$INCL_DIR."footer.php";
