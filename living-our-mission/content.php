ed607decea69249a2052977e5026bc4eace2f063
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-01 14:19:38 -0500

    updates to landing page

--- living-our-mission/content.php
+++ living-our-mission/content.php
@@ -1,31 +1,11 @@
 
 	<div class="column ten flex">
-	<div class="keeping-people-safe">
-			<div>
+		<div class="living-our-mission">
+		<div>
 			<img src="../img/RTI_w_1in.png" class="logo">
-			<h1>Keeping People Safe</h1>
-				<p class="light">At RTI, we tackle some of the toughest challenges facing the world today. Our clients rely on our objective research and evidence-based approach to help combat societal issues, including sexual assault, drug abuse, school violence, and extremism. Informed by data and committed to scientific rigor, we look at these critical issues through an objective lens to devise policies and practices that ensure safety for all.</p>
-			</div>
-			<div>
-				<p class="regular">Explore how we safeguard people and communities through our work:</p>
-				<ul>
-					<li><a href="training-targets-sexual-assault-investigation-improvements.php">Training Targets Sexual Assault Investigation Improvements</a></li>
-					<li><a href="securing-schools.php">Securing Schools</a></li>
-					<li><a href="studying-how-radicalism-develops.php">Studying How Radicalism Develops</a></li>
-					<li><a href="tackling-domestic-violence-with-technology.php">Tackling Domestic Violence with Technology</a></li>
-					<li><a href="uncovering-cannabis-product-risks.php">Uncovering Cannabis Product Risks</a></li>
-					<li><a href="reducing-police-stress.php">Reducing Police Stress</a></li>
-					<li><a href="exploring-a-new-weapon-in-opiod-crisis-communication.php">Exploring a New Weapon in Opioid Crisis: Communication</a></li>
-				</ul>
-			</div>
+			<h1><a href="/message-from-president/" target="_blank">The President's Message</a><br>
+			<a href="https://www.rti.org/about-us" target="_blank">About RTI</a></h1>
 		</div>
-		<!--<div class="living-our-mission">
-			<div>
-				<img src="../img/RTI_w_1in.png" class="logo">
-				<h1><a href="/message-from-president/" target="_blank">The President's Message</a><br>
-				<a href="https://www.rti.org/about-us" target="_blank">About RTI</a></h1>
-			</div>
-		</div>-->
 	</div>
 
 	
