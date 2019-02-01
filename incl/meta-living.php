2623f9bdf6696e0267b5a9e61fc52ddeacd1ca8b
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-01 15:22:35 -0500

    Update meta-living.php

--- incl/meta-living.php
+++ incl/meta-living.php
@@ -5,5 +5,5 @@
 </head>
 <body>
 <div class="container">
-<img src="test-1_HalfImages.gif" class="sectionBack">
+<img src="../img/test-1_HalfImages.gif" class="sectionBack">
 <div class="innerContainer" style="width:100%;">
