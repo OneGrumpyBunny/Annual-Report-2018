fe3baa5be3a1570645411825cca70551cb200ee1
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-01 13:35:50 -0500

    Update content.php

--- content.php
+++ content.php
@@ -1,9 +1,12 @@
 
 	<div class="column ten flex">
 		<div class="landingPage">
-			<h1 class="stretch">LIVING</h1>
-			<h2 class="stretch">our mission</h2>
-			<h3 class="stretch">2018 annual report</h3>
+			<div>
+				<img src="../img/RTI_w_1in.png" class="logo">
+				<h1 class="stretch">LIVING</h1>
+				<h2 class="stretch">our mission</h2>
+				<h3 class="stretch">2018 annual report</h3>
+			</div>
 		</div>
 	</div>
 	
