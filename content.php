2bab5b8f7d4ee9e08060864318a148bf4b93a28c
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-04 18:32:29 -0500

    mobile

--- content.php
+++ content.php
@@ -1,6 +1,15 @@
 
 	<div class="column ten flex">
-		<div class="landingPage">
+	<div class="landingPageMobile">
+				<img src="img/RTI_w_1in.png" class="logo">
+				<div>
+					<h1>LIVING</h1>
+					<h2>our mission</h2>
+					<h3>2018 annual report</h3>
+				</div>
+				<div><a href="" class="yellowlink light">A Message from the<br>President of RTI</a><br><br><a href="" class="light">Read More</a></div>
+			</div>
+			<div class="landingPage">
 			<div>
 				<img src="img/RTI_w_1in.png" class="logo">
 				<h1>LIVING</h1>
