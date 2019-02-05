b354b98be802934da5f51d638e1772807b8048e7
Gieck <cgieck@rti.org>
2019-02-05 13:01:45 -0500

    living our mission

--- living-our-mission/content.php
+++ living-our-mission/content.php
@@ -3,11 +3,10 @@
 		<div class="living-our-mission">
 			<div>
 				<img src="../img/RTI_w_1in.png" class="logo">
-				<h1>
-					<a href="/dev/message-from-president/">The President's Message</a>
-					<br/>
-					<a href="https://www.rti.org/about-us" target="_blank">About RTI</a>
-				</h1>
+				<ul>
+					<li><a href="/dev/message-from-president/">The President's Message</a></li>
+					<li><a href="https://www.rti.org/about-us" target="_blank">About RTI</a></li>
+				</ul>
 			</div>
 			<div>
 				
