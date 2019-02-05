538314d067d73a2beeee4494b10dac9625638bad
Gieck <cgieck@rti.org>
2019-02-05 13:59:55 -0500

    test

--- enhancing-health-through-science-and-practice/focusing-on-filipino-family-health.php
+++ enhancing-health-through-science-and-practice/focusing-on-filipino-family-health.php
@@ -5,7 +5,7 @@ include $REL_DIR."config.php";
 include $REL_DIR.$INCL_DIR."header.php";
 include $REL_DIR.$INCL_DIR."meta-health.php";
 ?>
-<img src="img/enhancing-health-back.png" class="sectionBack">
+<img src="img/test-sparkle.png" class="sectionBack">
 	<div class="innerContainer">
 <div class="column left flex">
     <div class="enhancing-health-page">
