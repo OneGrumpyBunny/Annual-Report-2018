6b91c742d57d37842223c28a4b20ae0330b64a30
Gieck <cgieck@rti.org>
2019-02-05 13:07:06 -0500

    test sparkle for maria

--- incl/meta-health.php
+++ incl/meta-health.php
@@ -5,5 +5,5 @@
 </head>
 <body>
 <div class="container">
-    <img src="../img/enhancing-health-back.png" class="sectionBack">
+    <img src="../img/test-sparkle.png" class="sectionBack">
 	<div class="innerContainer">
