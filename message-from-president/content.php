c14c19049fd438ae66b44618c8918f8b99936d4a
Gieck <cgieck@rti.org>
2019-02-05 12:44:59 -0500

    president message

--- message-from-president/content.php
+++ message-from-president/content.php
@@ -3,8 +3,16 @@
 		<div class="message-from-president">
 			<div>
 			<img src="../img/RTI_w_1in.png" class="logo">
-			<h1>A Letter from the President</h1>
-				<p class="light">At RTI, we tackle some of the toughest challenges facing the world today. Our clients rely on our objective research and evidence-based approach to help combat societal issues, including sexual assault, drug abuse, school violence, and extremism. Informed by data and committed to scientific rigor, we look at these critical issues through an objective lens to devise policies and practices that ensure safety for all.</p>
+			<h1>2018 Annual Report</h1>
+			<h2>A Letter from the President</h2>
+				<p class="light">
+					This year, RTI International marked a significant milestone as we began celebrating 60 years of living our mission. As I look back over the institute’s trajectory since 1958, I can see clearly how our commitment to improving the human condition has formed a bedrock for immense positive impact around the world. <br/><br/>
+					In 2018, we made great strides in research that improves people’s health and safeguards schools and communities. We supported efforts to advance education and bolster economic stability in developing nations. We explored science-based solutions to enable responsible stewardship of our planet. And because the commitment to living our mission runs deep, we donated significant time and resources in partnership with charitable and community organizations around the world. <br/><br/>
+					Moreover, this was one of the most successful years in our history. We pursued strategic business collaborations, spun out a new venture company, and set a record for staff contributions to scientific stature. We also opened the doors to Horizon, our new headquarters building in Research Triangle Park, NC, where creative ideas and cross-institute partnerships unfold every day. <br/><br/>
+					With a stellar year behind us, it is exciting to look ahead and imagine where our work will take us. I am proud of our 2018 accomplishments and, more important, our people, who live RTI’s mission every day to make the world a better place. <br/><br/>
+					Wayne E. Holden, President and CEO<br/>
+					RTI International
+				</p>
 			</div>
 			<div>
 				<ul>
