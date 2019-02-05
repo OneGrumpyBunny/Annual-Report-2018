1d55b74f7bbfd3f37d52dd01d3dfd0b9ba6cc19b
Gieck <cgieck@rti.org>
2019-02-05 12:32:36 -0500

    testing

--- message-from-president/content.php
+++ message-from-president/content.php
@@ -3,7 +3,7 @@
 		<div class="living-our-mission">
 			<div>
 				<img src="../img/RTI_w_1in.png" class="logo">
-				<h1><a href="/message-from-president/" target="_blank">The President's Message</a><br>
+				<h1><a href="/dev/message-from-president/" target="_blank">The President's Message</a><br>
 				<p class="light">Children will not thrive if they cannot read. Youth will not flourish without education or skills training. Countries will not prosper without electricity or infrastructure. From improving literacy rates for 7 million Kenyan children, to providing electricity access to 600 million people in sub-Saharan Africa, to offer training for the 20% of unemployed youth in Rwanda, we are living our mission every day to help communities around the world achieve a brighter future.</p>
 			</div>
 				<a href="https://www.rti.org/about-us" target="_blank">About RTI</a></h1>
