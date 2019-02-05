79bd97c72a6047ef2dda853930144d7430dd9be9
Gieck <cgieck@rti.org>
2019-02-05 12:41:51 -0500

    president message

--- living-our-mission/content.php
+++ living-our-mission/content.php
@@ -4,7 +4,7 @@
 			<div>
 				<img src="../img/RTI_w_1in.png" class="logo">
 				<ul>
-					<li><a href="/dev/message-from-president/" target="_blank">The President's Message</a></li>
+					<li><a href="/dev/message-from-president/">The President's Message</a></li>
 					<li><a href="https://www.rti.org/about-us" target="_blank">About RTI</a></li>
 				</ul>
 				
--- message-from-president/content.php
+++ message-from-president/content.php
@@ -3,7 +3,7 @@
 		<div class="message-from-president">
 			<div>
 			<img src="../img/RTI_w_1in.png" class="logo">
-			<h1>Message from President</h1>
+			<h1>A Letter from the President</h1>
 				<p class="light">At RTI, we tackle some of the toughest challenges facing the world today. Our clients rely on our objective research and evidence-based approach to help combat societal issues, including sexual assault, drug abuse, school violence, and extremism. Informed by data and committed to scientific rigor, we look at these critical issues through an objective lens to devise policies and practices that ensure safety for all.</p>
 			</div>
 			<div>
