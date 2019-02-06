97f60a7e2901efc2827a5fd063c2569cb9a69b5a
Gieck <cgieck@rti.org>
2019-02-06 12:34:34 -0500

    test ata a glance

--- about-rti/at-a-glance.php
+++ about-rti/at-a-glance.php
@@ -1,42 +1,65 @@
-<?php 
-$REL_DIR = "../";
-include $REL_DIR."config.php";
 
-include $REL_DIR.$INCL_DIR."header.php";
-?>
-<img src="img/living-our-mission-back.png" class="sectionBack">
-	<div class="innerContainer">
-<div class="column left flex">
-    <div class="thrive-and-prosper-page">
-
-    
-        <!--- update content below this line --->
-
-
-        <div>
-            <h1>A Message from our President</h1>
-            <!-- <h2>Decisions delayed; employment choices made based on the cost of education</h2> -->
-            <p class="light">
-					This year, RTI International marked a significant milestone as we began celebrating 60 years of living our mission. As I look back over the institute’s trajectory since 1958, I can see clearly how our commitment to improving the human condition has formed a bedrock for immense positive impact around the world.
+<img src="../img/test-1_HalfImages.gif" class="sectionBack">
+<!-- mobile navigation -->
+<header>
+	<a id="pull"><i class="fas fa-arrow-left"></i></a>
+	<nav class="nav-total">
+	<a href="#" class="nav-link">
+		<span class="side-border about"></span>
+		<span class="side-line" id="about"></span>
+		<p id="section-1" class="nav-item">
+			About RTI in 2018
+			</p>
+		</a>
+		<a href="#" class="nav-link">
+		<span class="side-border health"></span>
+		<span class="side-line" id="health"></span>
+		<p id="section-2" class="nav-item">
+			Enhancing<br>Healh
+		</p>
+		</a>
+		<a href="#" class="nav-link">
+		<span class="side-border safe"></span>
+		<span class="side-line" id="safe"></span>
+		<p id="section-2" class="nav-item">
+			Keeping<br>People Safe
+		</p>
+		</a>
+		<a href="#" class="nav-link">
+		<span class="side-border thrive"></span>
+		<span class="side-line" id="thrive"></span>
+		<p id="section-4" class="nav-item">
+			Helping Regions<br>Thrive and Prosper
+		</p>
+		</a>
+		<a href="#" class="nav-link">
+		<span class="side-border protect"></span>
+		<span class="side-line" id="protect"></span>
+		<p id="section-5" class="nav-item">
+			Protecting<br>the Planet
+		</p>
+		</a>
+	</nav>
+</header>
+<div class="innerContainer" style="width:100%;">
+	<div class="column left flex">
+		<div class="living-our-mission">
+			<div>
+				<img src="../img/RTI_w_1in.png" class="logo">
+				<p class="light">At RTI International, our work is driven by scientific rigor, objective research, and technical expertise. But the underlying force that propels us forward is an unwavering commitment to our mission: improving the human condition by turning knowledge into practice. Together, we explore new ideas and seek solutions to critical issues facing people everywhere. In our 2018 annual report, we invite you to learn how we are living our mission as we strive to deliver the promise of science for global good.
 				</p>
-            <!-- <p class="readmore"><a href="" target="_blank">READ MORE</a></p> -->
-        </div>
-        <div>
-            <!-- <p class="regular">Learn more about how we are helping regions thrive and prosper:</p> -->
-            <ul>
-                <li class="active"><!--<a href="message-from-president.php">-->A Message from our President<!--</a>--></li>
-                <li><a href="at-a-glance.php">RTI at a Glance</a></li>
-                <li><a href="executive-leadership.php">Meet RTI's Executive Leadership</a></li>
-                <li><a href="board-of-governors.php">Meet RTI's Board of Governors</a></li>
-            </ul>
-        </div>
+				<ul>
+					<li><a href="message-from-president.php">A Message from our President</a></li>
+					<li class="active"><a href="at-a-glance.php">RTI at a Glance</a></li>
+					<li><a href="executive-leadership.php">Meet RTI's Executive Leadership</a></li>
+					<li><a href="board-of-governors.php">Meet RTI's Board of Governors</a></li>
+            	</ul>
+			</div>
+			<div>
+				
+			</div>
+		</div>
+	</div>
 
-        <!--- end variable content --->
-
-    </div>
-</div>
-<?php
-include $REL_DIR.$INCL_DIR."nav.php";
-include $REL_DIR.$INCL_DIR."footer.php";
 	
-?>
+
