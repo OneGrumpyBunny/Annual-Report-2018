f296fc69d4826b9548d9179ae6a51009ee66cf45
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-06 14:32:22 -0500

    mobile landing pages

--- incl/meta-health.php
+++ incl/meta-health.php
@@ -4,4 +4,4 @@
 
 </head>
 <body>
-<div class="container">
+<div class="container enhancing-back">
--- incl/mobile-nav.php
+++ incl/mobile-nav.php
@@ -3,35 +3,35 @@
 	<header>
 		<a id="pull"><i class="fas fa-arrow-left"></i></a>
 		<nav class="nav-total">
-		<a href="#" class="nav-link">
+		<a href="about-rti/" class="nav-link">
 			<span class="side-border about"></span>
 			<span class="side-line" id="about"></span>
 			<p id="section-1" class="nav-item">
 				2018<br>Annual Report
 				</p>
 			</a>
-			<a href="enhancinghealth.html" class="nav-link">
+			<a href="enhancing-health-through-science-and-practice/" class="nav-link">
 			<span class="side-border health"></span>
 			<span class="side-line" id="health"></span>
 			<p id="section-2" class="nav-item">
 				Enhancing<br>Healh
 			</p>
 			</a>
-			<a href="#" class="nav-link">
+			<a href="keeping-people-safe/" class="nav-link">
 			<span class="side-border safe"></span>
 			<span class="side-line" id="safe"></span>
 			<p id="section-2" class="nav-item">
 				Keeping<br>People Safe
 			</p>
 			</a>
-			<a href="#" class="nav-link">
+			<a href="helping-regions-thrive-and-prosper/" class="nav-link">
 			<span class="side-border thrive"></span>
 			<span class="side-line" id="thrive"></span>
 			<p id="section-4" class="nav-item">
 				Helping Regions<br>Thrive and Prosper
 			</p>
 			</a>
-			<a href="#" class="nav-link">
+			<a href="protecting-the-planet/" class="nav-link">
 			<span class="side-border protect"></span>
 			<span class="side-line" id="protect"></span>
 			<p id="section-5" class="nav-item">
