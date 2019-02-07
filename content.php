e0225e69c4d2bc0b852a1d38a78b52f7823b2ee9
Gieck <cgieck@rti.org>
2019-02-07 15:39:17 -0500

    stretch

--- content.php
+++ content.php
@@ -26,9 +26,9 @@
 			<div class="landingPage">
 			<div>
 				<a href="http://www.rti.org"><img src="./img/RTI_w_1in.png" class="logo"></a>
-				<h1 class="stretch one">LIVING</h1>
-				<h2 class="stretch two">our mission</h2>
-				<h3 class="stretch three">2018 annual report</h3>
+				<h1 class="stretch">LIVING</h1>
+				<h2 class="stretch">our mission</h2>
+				<h3 class="stretch">2018 annual report</h3>
 			</div>
 		</div>
 	</div>
