6918fb9829e731217293d22e75c5d1017131b8b2
Gieck <cgieck@rti.org>
2019-02-06 19:58:24 -0500

    adding share links

--- incl/main-footer.php
+++ incl/main-footer.php
@@ -3,9 +3,9 @@
         <p>RTI International is an independent, nonprofit research institute dedicated to improving the human condition. Clients rely on us to answer questions that demand an objective and multidisciplinary approach—one that integrates expertise across the social and laboratory sciences, engineering, and international development. We believe in the promise of science, and we are inspired every day to deliver on that promise for the good of people, communities, and businesses around the world.</p>
     </div>
     <div class= "icons">
-        <a href="" target="_blank"><i class="fab fa-twitter-square"></i></a>
-        <a href="" target="_blank"><i class="fab fa-linkedin"></i></a>
-        <a href="" target="_blank"><i class="fab fa-facebook-square"></i></a>
+        <a href="https://twitter.com/home?status=Learn%20how%20%40RTI_Intl%20is%20living%20their%20mission%20by%20improving%20the%20human%20condition%20http%3A//annualreport.rti.org/2018" target="_blank"><i class="fab fa-twitter-square"></i></a>
+        <a href="https://www.linkedin.com/shareArticle?mini=true&url=http%3A//annualreport.rti.org/2018&title=RTI%20Annual%20Report%202018&summary=&source=" target="_blank"><i class="fab fa-linkedin"></i></a>
+        <a href="https://www.facebook.com/sharer/sharer.php?u=http%3A//annualreport.rti.org/2018" target="_blank"><i class="fab fa-facebook-square"></i></a>
     </div>
 </div>
 
