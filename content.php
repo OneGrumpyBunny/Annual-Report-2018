e45a9bb9da04167850486f5fc7144d4dc05e56e5
Gieck <cgieck@rti.org>
2019-02-07 16:43:33 -0500

    stretch

--- content.php
+++ content.php
@@ -25,7 +25,7 @@
 		<div class="column left">
 			<div class="landingPage">
 				<a href="http://www.rti.org"><img src="./img/RTI_w_1in.png" class="logo"></a>
-				<div style="display: flex; flex-flow: column; flex: 1 0 77%; margin-left: 23%;">
+				<div style="display: flex; flex-flow: column; flex: 1 0 60%; margin-left: 15%;">
 					<h1 class="stretch">LIVING</h1>
 					<h2 class="stretch">our mission</h2>
 					<h3 class="stretch">2018 annual report</h3>
