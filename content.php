b1158ebdfe269b2c986b38f34039fcae7f5afbb5
Gieck <cgieck@rti.org>
2019-02-07 11:59:20 -0500

    flex

--- content.php
+++ content.php
@@ -24,7 +24,8 @@
 	<div class="innerContainer">
 		<div class="column left">
 			<div class="landingPage">
-			<div style="justify-content:space-evenly;">
+			<!-- <div style="justify-content:space-evenly;"> -->
+			<div>
 				<a href="http://www.rti.org"><img src="./img/RTI_w_1in.png" class="logo"></a>
 					<h1 class="stretch">LIVING</h1>
 					<h2 class="stretch">our mission</h2>
