ad53d770c5865311337b50c59e2a9fb78bf80d23
Gieck <cgieck@rti.org>
2019-02-07 10:19:18 -0500

    testing footer

--- incl/main-footer.php
+++ incl/main-footer.php
@@ -1,6 +1,7 @@
 <div class="footer-text">
-    <div style="flex: 0 0 85%">
+    <div class="text">
         <p>RTI International is an independent, nonprofit research institute dedicated to improving the human condition. Clients rely on us to answer questions that demand an objective and multidisciplinary approach—one that integrates expertise across the social and laboratory sciences, engineering, and international development. We believe in the promise of science, and we are inspired every day to deliver on that promise for the good of people, communities, and businesses around the world.</p>
+        <p>RTI International is a registered trademark and a trade name of Research Triangle Institute. The RTI logo is a registered trademark of Research Triangle Institute.</p>
     </div>
     <div class= "icons">
         <a href="https://twitter.com/home?status=Learn%20how%20%40RTI_Intl%20is%20living%20their%20mission%20by%20improving%20the%20human%20condition%20http%3A//annualreport.rti.org/2018" target="_blank"><i class="fab fa-twitter-square"></i></a>
