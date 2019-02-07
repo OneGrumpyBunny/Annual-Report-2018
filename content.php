37fdee73379b9c55516ae78b17fcf4d0e2e2f67b
Gieck <cgieck@rti.org>
2019-02-06 19:10:50 -0500

    footer

--- content.php
+++ content.php
@@ -35,6 +35,9 @@
 				<p>RTI International is an independent, nonprofit research institute dedicated to improving the human condition. Clients rely on us to answer questions that demand an objective and multidisciplinary approach—one that integrates expertise across the social and laboratory sciences, engineering, and international development. We believe in the promise of science, and we are inspired every day to deliver on that promise for the good of people, communities, and businesses around the world.</p>
 			</div>
 			<div class= "icons">
+				<a href="" target="_blank"><i class="fab fa-twitter-square"></i></a>
+				<a href="" target="_blank"><i class="fab fa-linkedin"></i></a>
+				<a href="" target="_blank"><i class="fab fa-facebook-square"></i></a>
 			</div>
 		</div>
 	</div>
--- incl/header.php
+++ incl/header.php
@@ -14,7 +14,7 @@
     <link rel='stylesheet' href='<?php echo $REL_DIR.$CSS_DIR ?>mobile.css' />
     <link rel='stylesheet' href='<?php echo $REL_DIR.$CSS_DIR ?>accordion.css' />
     
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-6jHF7Z3XI3fF4XZixAuSu0gGKrXwoX/w3uFPxC56OtjChio7wtTGJWRW53Nhx6Ev" crossorigin="anonymous">
     
 	<script type='text/javascript' src='<?php echo $REL_DIR.$JS_DIR ?>jquery.min.js'></script>
 	<!--<script type='text/javascript' src='<?php echo $REL_DIR.$JS_DIR ?>stretchtext.js'></script>-->
