e50fc26e9ec73d5915c070ffd6689bd92a6391bd
Gieck <cgieck@rti.org>
2019-02-06 19:28:08 -0500

    footer

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ include "config.php";
 include $INCL_DIR."header.php";
 include $INCL_DIR."meta.php";
 include "content.php";
-include $INCL_DIR."nav.php";
 include $INCL_DIR."main-footer.php";
+include $INCL_DIR."nav.php";
 include $INCL_DIR."footer.php";
 	
 ?>
