afb9b111a9f6f7ddd47ee85d6dbb2b788143abd7
Gieck <cgieck@rti.org>
2019-02-07 16:04:22 -0500

    flex main pagE

--- content.php
+++ content.php
@@ -24,13 +24,13 @@
 	<div class="innerContainer">
 		<div class="column left">
 			<div class="landingPage">
-			<div>
 				<a href="http://www.rti.org"><img src="./img/RTI_w_1in.png" class="logo"></a>
-				<h1 class="stretch">LIVING</h1>
-				<h2 class="stretch">our mission</h2>
-				<h3 class="stretch">2018 annual report</h3>
+				<div style="display: flex; flex-flow: column; flex: 0 0 60%;">
+					<h1 class="stretch">LIVING</h1>
+					<h2 class="stretch">our mission</h2>
+					<h3 class="stretch">2018 annual report</h3>
+				</div>
 			</div>
 		</div>
-	</div>
 	
 
