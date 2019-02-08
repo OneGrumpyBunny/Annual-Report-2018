d1c1602e2b8c0d3fb66538fbce6f06a4a990fd7e
Gieck <cgieck@rti.org>
2019-02-08 15:34:49 -0500

    nbsp

--- protecting-the-planet/working-to-provide-clean-water-in-nigeria.php
+++ protecting-the-planet/working-to-provide-clean-water-in-nigeria.php
@@ -16,9 +16,9 @@ include $REL_DIR.$INCL_DIR."meta-planet.php";
 
         <div>
         <a href="http://www.rti.org"><img src="../img/RTI_w_1in.png" class="logo"></a>
-            <h1>Effective Water, Sanitation, and Hygiene Services in Nigeria (E-WASH)</h1>
-            <h2>Working with state water boards to deliver better urban services to the Nigerian people</h2>
-            <p class="light">Of Nigeria’s population of nearly 300 million people, 71 million do not have access to clean water and 130 million do not have access to basic sanitation. This lack of water, sanitation, and hygiene services—exacerbated by accelerated urbanization, poor cost recovery, and weak governance and institutional frameworks—adversely affects Nigerian citizens’ health, as well as their access to educational and economic opportunities and their work efficiency and labor productivity.</p>
+            <h1>Effective Water, Sanitation, and Hygiene Services in Nigeria &nbsp;E-WASH)</h1>
+            <h2>Working with state water boards to deliver better urban services to the Nigerian&nbsp;people</h2>
+            <p class="light">Of Nigeria’s population of nearly 300 million people, 71 million do not have access to clean water and 130 million do not have access to basic sanitation. This lack of water, sanitation, and hygiene services—exacerbated by accelerated urbanization, poor cost recovery, and weak governance and institutional frameworks—adversely affects Nigerian citizens’ health, as well as their access to educational and economic opportunities and their work efficiency and labor&nbsp;productivity.</p>
             <p class="readmore"><a href="https://www.rti.org/impact/effective-water-sanitation-and-hygiene-services-nigeria-e-wash" target="_blank">READ MORE</a></p>
         </div>
         <div>
