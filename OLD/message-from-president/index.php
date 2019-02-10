a868426a2d1b2729671ef200f883fa99748d28c8
Gieck <cgieck@rti.org>
2019-02-10 09:05:29 -0500

    gitignoring

---
+++ OLD/message-from-president/index.php
@@ -0,0 +1,11 @@
+<?php 
+$REL_DIR = "../";
+include $REL_DIR."config.php";
+
+include $REL_DIR.$INCL_DIR."header.php";
+include $REL_DIR.$INCL_DIR."meta-president.php";
+include "content.php";
+include $REL_DIR.$INCL_DIR."nav.php";
+include $REL_DIR.$INCL_DIR."footer.php";
+	
+?>
