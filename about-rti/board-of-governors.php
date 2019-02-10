7f5439d2b60c1ea1f289218bf525b9a0b59aae9d
Gieck <cgieck@rti.org>
2019-02-10 08:38:20 -0500

    testing

--- about-rti/board-of-governors.php
+++ about-rti/board-of-governors.php
@@ -6,7 +6,7 @@ include $REL_DIR.$INCL_DIR."header.php";
 include $REL_DIR.$INCL_DIR."meta-living.php";
 ?>
 
-<img src="img/filter-back.png" class="sectionBack">
+<img src="img/filter-back.png" class="sectionBack" style="height:100%;">
 	<div class="innerContainer">
 <div class="column left flex">
     <div class="living-our-mission-page">
