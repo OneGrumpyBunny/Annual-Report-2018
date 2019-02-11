965b303a9a8933824c2a3fe01e3f071b02ea1ecc
Gieck <cgieck@rti.org>
2019-02-11 14:58:08 -0500

    filter

--- about-rti/at-a-glance.php
+++ about-rti/at-a-glance.php
@@ -5,7 +5,7 @@ include $REL_DIR."config.php";
 include $REL_DIR.$INCL_DIR."header.php";
 include $REL_DIR.$INCL_DIR."meta-living.php";
 ?>
-<img src="img/at-a-glance-back.png" class="sectionBack">
+<img src="img/filter-back.png" class="sectionBack">
 	<div class="innerContainer">
 <div class="column left flex">
     <div class="living-our-mission-page">
@@ -17,13 +17,16 @@ include $REL_DIR.$INCL_DIR."meta-living.php";
         <div>
         <a href="http://www.rti.org"><img src="../img/RTI_w_1in.png" class="logo"></a>
         <!-- <img src="../img/RTI_w_1in.png" class="logo"> -->
-            <!---<h1>At a Glance</h1>--->
+            <h1>At a Glance</h1>
             <!-- <h2>Decisions delayed; employment choices made based on the cost of education</h2> -->
             <!---<p class="light">
 					Lorem
 				</p>--->
             <!-- <p class="readmore"><a href="" target="_blank">READ MORE</a></p> -->
             <!---<div style="background-color: black; width: 100%; min-height: 100px;"></div>--->
+            <div id="at-a-glance-box">
+                <img src="img/at-a-glance.png" id="at-a-glance-img">
+            </div>
         </div>
         <div>
             <!-- <p class="regular">Learn more about how we are helping regions thrive and prosper:</p> -->
