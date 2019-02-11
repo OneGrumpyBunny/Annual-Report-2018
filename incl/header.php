8d021f28d567b42cc7216c2666863088edc6fc81
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-11 16:41:59 -0500

    favicon

--- incl/header.php
+++ incl/header.php
@@ -18,6 +18,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <meta description="Annual Report 2018"> -->
     
+    <link rel="shortcut icon" href="/2018/img/favicon.png" type="image/png" />
+
     <link rel="stylesheet" href="https://use.typekit.net/vxh7raq.css">
     <link rel='stylesheet' href='<?php echo $REL_DIR.$CSS_DIR ?>normalize.css' />
     <link rel='stylesheet' href='<?php echo $REL_DIR.$CSS_DIR ?>styles.css' />
