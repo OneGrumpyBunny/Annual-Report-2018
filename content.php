4640ff9afb1271f4c58ed2579af7c0817d51c735
Gieck <cgieck@rti.org>
2019-02-12 11:50:07 -0500

    removing extra div

--- content.php
+++ content.php
@@ -17,7 +17,9 @@
 				<h2 style="letter-spacing: calc(5px + (5 - 5) * ((100vw - 100px) / (1600 - 450)))">our mission</h2>
 				<h3 style="letter-spacing: calc(3px + (3 - 3) * ((100vw - 100px) / (1600 - 450)));">2018 annual report</h3>
 			</div>
-			<div><a href="about-rti/index.php?ex=y" class="yellowlink light">A Message from the<br>President of RTI</a><br><br><a href="about-rti/" class="light">Read More</a></div>
+			<div>
+				<a href="about-rti/index.php?ex=y" class="yellowlink light">A Message from the<br>President of RTI</a><br><br><a href="about-rti/" class="light">Read More</a>
+			</div>
 			<div class="copyright">
 				<p>RTI International is an independent, nonprofit research institute dedicated to improving the human condition. Clients rely on us to answer questions that demand an objective and multidisciplinary approach—one that integrates expertise across the social and laboratory sciences, engineering, and international development. We believe in the promise of science, and we are inspired every day to deliver on that promise for the good of people, communities, and businesses around the world.</p>
 				<p>RTI International is a registered trademark and a trade name of Research Triangle Institute. The RTI logo is a registered trademark of Research Triangle Institute.</p>
@@ -27,9 +29,7 @@
 					<a href="https://www.facebook.com/sharer/sharer.php?u=http%3A//annualreport.rti.org/2018" target="_blank"><i class="fab fa-facebook-square"></i></a>
 				</div>
 			</div>
-	</div>
-		
-	</div>
+		</div>
 	</div>
 	<!-- hidden on mobile -->
 	<div class="innerContainer">
