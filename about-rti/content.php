8c02f70310b4000fa4abb3b1c79b6920c4906ca0
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-12 16:32:31 -0500

    bog

--- about-rti/content.php
+++ about-rti/content.php
@@ -95,42 +95,56 @@
                     <li><strong>Harry L. Smith&nbsp;Jr.</strong>
                                 <br>
                                 Businessman, Representative of the Board, The University of North Carolina&nbsp;System</li>
-                    <li><strong>Peter M. Scott&nbsp;III&nbsp;(Chair)</strong>
-                                <br>
-                                Former CFO, Progress Energy; Former President and CEO, Progress Energy Services&nbsp;Company</li>
-                    <li><strong>Thomas F.&nbsp;Darden</strong>
-                                <br>
-                                President and Chief Executive Officer, Cherokee Investment&nbsp;Partners</li>
-                    <li><strong>Robert A.&nbsp;Ingram</strong>
-                                <br>
-                                General Partner, Hatteras Venture Partners; Former CEO,&nbsp;GlaxoWellcome</li>
-                    <li><strong>Terry&nbsp;Magnuson</strong>
-                                <br>
-                                Vice Chancellor for Research, University of North Carolina at Chapel&nbsp;Hill</li>
-                    <li><strong> W.G. Champion&nbsp;Mitchell</strong><br>
-                                Former CEO, Network&nbsp;Solutions</li>
-                    <li><strong>William M. Moore&nbsp;Jr.</strong>
-                                <br>
-                                Retired CEO, Trident Financial&nbsp;Corporation</li>
-                    <li><strong>Alan H.&nbsp;Rebar</strong>
-                                <br>
-                                Vice Chancellor for Research, Innovation and Economic Development, North Carolina State&nbsp;University</li>
-                    <li><strong>Clinton P.&nbsp;Carter</strong>
-                                <br>
-                                Senior Vice President for Finance & Administration and Chief Financial Officer, The University of North Carolina&nbsp;System</li>
+                </ul>
+                <h2>Board of Governors</h2>
+
+                <ul id="bog-list">
                     <li><strong>Larry&nbsp;Carin</strong>
                                 <br>
                                 Vice Provost of Research, Duke&nbsp;University</li>
+
+                    <li><strong>Clinton P.&nbsp;Carter</strong>
+                                <br>
+                                Senior Vice President for Finance & Administration and Chief Financial Officer, The University of North Carolina&nbsp;System</li>
+
+                    <li><strong>Thomas F.&nbsp;Darden</strong>
+                                <br>
+                                President and Chief Executive Officer, Cherokee Investment&nbsp;Partners</li>
+
                     <li><strong><a href="https://www.rti.org/expert/e-wayne-holden" targer="_blank" style="color:#99B1C5">E. Wayne Holden</a></strong><br>
                                 President and CEO, RTI&nbsp;International</li>
+
+                    <li><strong>Robert A.&nbsp;Ingram</strong>
+                                <br>
+                                General Partner, Hatteras Venture Partners; Former CEO,&nbsp;GlaxoWellcome</li>
+
                     <li><strong> Peter&nbsp;Lange</strong><br>
                                 Thomas&nbsp;A.&nbsp;Langford&nbsp;University Professor of Political Science and Public Policy, Provost Emeritus, Duke&nbsp;University</li>
+
+                    <li><strong>Terry&nbsp;Magnuson</strong>
+                                <br>
+                                Vice Chancellor for Research, University of North Carolina at Chapel&nbsp;Hill</li>
+
                     <li><strong>Harold L. Martin&nbsp;Sr.</strong><br>
                                 Chancellor, North Carolina A&T State&nbsp;University</li>
+
+                    <li><strong> W.G. Champion&nbsp;Mitchell</strong><br>
+                                Former CEO, Network&nbsp;Solutions</li>
+
                     <li><strong>John H.&nbsp;Moellering</strong><br>
                                 Chairman Emeritus,&nbsp;USAA</li>
+                                
+                    <li><strong>William M. Moore&nbsp;Jr.</strong>
+                                <br>
+                                Retired CEO, Trident Financial&nbsp;Corporation</li>
+
                     <li><strong>Hilda&nbsp;Pinnix-Ragland</strong><br>
                                 Former Energy Executive, Duke Energy; Managing Director, AHK Global Resources,&nbsp;LLC</li>
+
+                    <li><strong>Peter M. Scott&nbsp;III&nbsp;(Chair)</strong>
+                                <br>
+                                Former CFO, Progress Energy; Former President and CEO, Progress Energy Services&nbsp;Company</li>
+
                     <li><strong>Christy&nbsp;Shaffer</strong><br>
                                 General Partner, Hatteras Venture&nbsp;Partners</li>
                 </ul> 
