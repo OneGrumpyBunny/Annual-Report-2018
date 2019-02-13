5133982d139bced1a5ecf0fe459a4dd1c5736958
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-13 08:50:11 -0500

    read more link

--- protecting-the-planet/content.php
+++ protecting-the-planet/content.php
@@ -8,14 +8,14 @@
 		<p class="mobileHead mobileHeadPlanet">Protecting the Planet</p>
 	</div>
 	<div class="innerContainerMob planetMob">
-		<p class="headerText">From reducing carbon emissions to developing renewable energy sources to ensuring clean drinking water, the challenges facing the environment affect us all as citizens of the planet and require science-based solutions that span social science, laboratory research, engineering, and program delivery. </p><p class="headerText hideMore">Our multidisciplinary approach ensures that the answers we need for all our tomorrows can be uncovered today.</p><p><span class="readmore expand">READ MORE</span><br><span class="readmore collapse hide">READ LESS</span></p>
+		<p class="headerText">From reducing carbon emissions to developing renewable energy sources to ensuring clean drinking water, the challenges facing the environment affect us all as citizens of the planet and require science-based solutions that span social science, laboratory research, engineering, and program delivery. </p><p class="headerText hideMore">Our multidisciplinary approach ensures that the answers we need for all our tomorrows can be uncovered today.</p><p><span class="readmore expand">Read More on RTI.org</span><br><span class="readmore collapse hide">READ LESS</span></p>
 		<section>
 			<div id="section1Head" class="acc-head" data-section="section1">
 				<p>Enlisting Citizen Scientists to Help Improve Air Quality</p><i class="fas fa-chevron-down crc-chevron" data-section="section1"></i>
 			</div>
 			<div id="section1" data-section="section1" class="acc-body">
 				<p>At any given moment, the National Aeronautics and Space Administration (NASA) has about two dozen Earth Observation Satellites in orbit. RTI is working with NASA&mdash;as well as the Universities Space Research Association, the South Coast Air Quality Management District, the Environmental Protection Agency, and various other institutions&mdash;to deploy low-cost air-quality monitors at ground level, the data from which can be used to provide additional information to help improve satellite readings.</p>
-				<p class="readmore"><a href="https://www.rti.org/impact/improving-nasa-satellite-data-low-cost-sensors" target="_blank">READ MORE </a>
+				<p class="readmore"><a href="https://www.rti.org/impact/improving-nasa-satellite-data-low-cost-sensors" target="_blank">Read More on RTI.org </a>
 				<button style="float:right;" class="crc-close1 close" data-section="section1"><!--<i class="fas fa-times"></i>-->Close</button></p>
 			</div>
 
@@ -24,7 +24,7 @@
 			</div>
 			<div id="section2" data-section="section2" class="acc-body">
 				<p>With participation from more than 300 representatives in the public and private sectors, academia, indigenous organizations, cooperatives, and organized groups, RTI is working with the Government of Guatemala to formulate the country’s first low-emission development strategy.</p>
-				<p class="readmore"><a href="https://www.rti.org/impact/guatemala-low-emission-development-strategy-g-leds" target="_blank">READ MORE </a>
+				<p class="readmore"><a href="https://www.rti.org/impact/guatemala-low-emission-development-strategy-g-leds" target="_blank">Read More on RTI.org </a>
 				<button style="float:right;" class="crc-close2 close" data-section="section2"><!--<i class="fas fa-times"></i>-->Close</button></p>
 			</div>
 
@@ -33,7 +33,7 @@
 			</div>
 			<div id="section3" data-section="section3" class="acc-body">
 				<p>Of Nigeria’s population of nearly 300 million people, 71 million do not have access to clean water and 130 million do not have access to basic sanitation. This lack of water, sanitation, and hygiene services—exacerbated by accelerated urbanization, poor cost recovery, and weak governance and institutional frameworks—adversely affects Nigerian citizens’ health, as well as their access to educational and economic opportunities and their work efficiency and labor productivity.</p>
-				<p class="readmore"><a href="https://www.rti.org/impact/effective-water-sanitation-and-hygiene-services-nigeria-e-wash" target="_blank">READ MORE </a>
+				<p class="readmore"><a href="https://www.rti.org/impact/effective-water-sanitation-and-hygiene-services-nigeria-e-wash" target="_blank">Read More on RTI.org </a>
 				<button style="float:right;" class="crc-close3 close" data-section="section3"><!--<i class="fas fa-times"></i>-->Close</i></button></p>
 			</div>
 
@@ -42,7 +42,7 @@
 			</div>
 			<div id="section4" data-section="section4" class="acc-body">
 				<p>As climate change continues to pose a threat, curbing the use of fossil fuels is more important than ever. The world is experiencing the largest energy transformation since the Industrial Revolution, and hydropower has been a forefront in this revolution.</p>
-				<p class="readmore"><a href="https://www.rti.org/insights/hydropower-wave-renewable-energy" target="_blank">READ MORE </a>
+				<p class="readmore"><a href="https://www.rti.org/insights/hydropower-wave-renewable-energy" target="_blank">Read More on RTI.org </a>
 				<button style="float:right;" class="crc-close4 close" data-section="section4"><!--<i class="fas fa-times"></i>-->Close</i></button></p>
 			</div>
 
@@ -51,7 +51,7 @@
 			</div>
 			<div id="section5" data-section="section5" class="acc-body">
 				<p>In the Philippines, various policy, technical, and financial barriers to clean energy usage, as well as a sustained increase of petroleum consumption in the transport sector, have made this island nation a good example of the coordinated strategies that must be implemented in the face of increased energy usage and fossil fuel consumption.</p>
-				<p class="readmore"><a href="https://www.rti.org/impact/b-leaders-building-low-emission-alternatives-develop-economic-resilience-and-sustainability" target="_blank">READ MORE </a>
+				<p class="readmore"><a href="https://www.rti.org/impact/b-leaders-building-low-emission-alternatives-develop-economic-resilience-and-sustainability" target="_blank">Read More on RTI.org </a>
 				<button style="float:right;" class="crc-close5 close" data-section="section5"><!--<i class="fas fa-times"></i>-->Close</i></button></p>
 			</div>
 
@@ -60,7 +60,7 @@
 			</div>
 			<div id="section6" data-section="section6" class="acc-body">
 				<p>The class of chemicals known as PFAS—per- and polyfluoroalkyl substances—poses a unique threat to public health that our chemical and environmental engineers are working to solve. These include PFOA; PFOS; and more than 3,000 related chemicals, including the GenX variant known for polluting the Cape Fear River in southeastern North Carolina. The Department of Defense’s Strategic Environmental Research and Development Program is looking for a solution and began working with RTI in mid-2018 to investigate a method of breaking down PFAS.</p>
-				<p class="readmore"><a href="https://www.rti.org/impact/new-method-destruction-perfluorinated-contaminants-found-water-supplies" target="_blank">READ MORE </a>
+				<p class="readmore"><a href="https://www.rti.org/impact/new-method-destruction-perfluorinated-contaminants-found-water-supplies" target="_blank">Read More on RTI.org </a>
 				<button style="float:right;" class="crc-close6 close" data-section="section6"><!--<i class="fas fa-times"></i>-->Close</i></button></p>
 			</div>
 		</section>
