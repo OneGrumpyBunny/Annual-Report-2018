16f8c2534f34ab47d4ea1c715cd0e7e0db8fee9b
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-15 13:38:44 -0500

    Update main-footer.php

--- incl/main-footer.php
+++ incl/main-footer.php
@@ -14,7 +14,23 @@
 </div> <!-- end inner container -->
 
 </div> <!-- end container -->
+<script type="text/javascript">
+piAId = '252812';
+piCId = '13862';
+piHostname = 'pi.pardot.com';
+
+(function() {
+    function async_load(){
+                    var s = document.createElement('script'); s.type = 'text/javascript';
+                    s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+                    var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+    }
+    if(window.attachEvent) { window.attachEvent('onload', async_load); }
+    else { window.addEventListener('load', async_load, false); }
+})();
+</script>
 
 <?php session_destroy();?>
+
 </body>
 </html>
