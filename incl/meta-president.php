4fb11624e933125279d4db53ea17c018e8ea57d7
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-20 17:21:39 -0500

    IE meta

--- incl/meta-president.php
+++ incl/meta-president.php
@@ -4,6 +4,19 @@
 
 </head>
 <body class="about-back">
+<!-- this is a message asking IE users to view the site in a different browser -->
+      
+<div id="oldIE" style = "display:none;position: absolute; top: 0; left: 0; background-color: #ffffff; padding: 0 2em;z-index:1000;">
+		   <p style= "text-align: center; font-size: 1.5 rem; color: #000;">
+		   		You're using an old out of date browser. To view this page properly, we recommend using a moderns, standards-compliant browser - such as <a href="https://support.microsoft.com/en-us/help/17621/internet-explorer-downloads">Microsoft Edge</a>, <a href="https://www.mozilla.org/en-US/firefox/">Firefox</a>, <a href="https://support.apple.com/downloads/safari">Safari</a>, or <a href="https://www.google.com/chrome/">Google Chrome</a>.
+                 <!--This site works best in modern web browsers like Microsoft Edge, Google Chrome, or Firefox. Your Internet Explorer web browser does not support current web standards and <a href="https://www.microsoft.com/en-us/windowsforbusiness/end-of-ie-support" target="_blank">has been replaced by Microsoft with Edge</a>.-->
+           </p>
+	  </div>
+	  <div id="quirks" style = "display:none;position: absolute; top: 0; left: 0; background-color: #ff0000; padding: 0 2em;z-index:1000;">
+           <p style= "text-align: center; font-size: 1.5 rem; color: #000;">
+                It looks like you have enabled Internet Explorer Compabitility View. The RTI Annual Report is optimized for browsers in standard mode. Please turn off compatibility view to maximize your browsing experience.
+           </p>
+      </div>
 <div class="container">
 		<div id="animBack"></div>
 		<div id="animBack2"></div>
