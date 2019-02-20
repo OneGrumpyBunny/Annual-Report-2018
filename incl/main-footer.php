851df898541cc48b8ea38107a2d66d6a08be7eab
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-20 17:00:31 -0500

    Update main-footer.php

--- incl/main-footer.php
+++ incl/main-footer.php
@@ -1,15 +1,11 @@
 
 </div> <!-- end inner container -->
 
-</div> <!-- end container -->
 <div class="footer-text">
     <div class="text">
-    <!--[if IE]>
-        <div text-align: left !important;> <![endif]-->
         <p>
-                RTI International is an independent, nonprofit research institute dedicated to improving the human condition. Clients rely on us to answer questions that demand an objective and multidisciplinary approach—one that integrates expertise across the social and laboratory sciences, engineering, and international development. We believe in the promise of science, and we are inspired every day to deliver on that promise for the good of people, communities, and businesses around the world.
+            RTI International is an independent, nonprofit research institute dedicated to improving the human condition. Clients rely on us to answer questions that demand an objective and multidisciplinary approach—one that integrates expertise across the social and laboratory sciences, engineering, and international development. We believe in the promise of science, and we are inspired every day to deliver on that promise for the good of people, communities, and businesses around the world.
         </p>
-        <!--[if IE]></div><![endif]-->
     </div>
     <div class= "icons">
         <p id="share">Share: </p>
@@ -17,8 +13,9 @@
         <a href="https://www.linkedin.com/shareArticle?mini=true&url=http%3A//annualreport.rti.org/2018&title=RTI%20Annual%20Report%202018&summary=&source=" target="_blank"><i class="fab fa-linkedin"></i></a>
         <a href="https://www.facebook.com/sharer/sharer.php?u=http%3A//annualreport.rti.org/2018" target="_blank"><i class="fab fa-facebook-square"></i></a>
     </div>
-</div>
+</div> <!-- end footer-text -->
 
+</div> <!-- end container -->
 <script type="text/javascript">
 piAId = '252812';
 piCId = '13862';
