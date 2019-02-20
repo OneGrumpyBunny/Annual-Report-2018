4c8c94e07756b431a76ceccc48ccc2a8b7451c8e
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-20 15:49:04 -0500

    Update main-footer.php

--- incl/main-footer.php
+++ incl/main-footer.php
@@ -13,7 +13,7 @@
       <!-- [if !IE]> 
         <p>RTI International is an independent, nonprofit research institute dedicated to improving the human condition. Clients rely on us to answer questions that demand an objective and multidisciplinary approach—one that integrates expertise across the social and laboratory sciences, engineering, and international development. We believe in the promise of science, and we are inspired every day to deliver on that promise for the good of people, communities, and businesses around the world.</p>
         <p>RTI International is a registered trademark and a trade name of Research Triangle Institute. The RTI logo is a registered trademark of Research Triangle Institute.</p> 
-    <![endif]-->
+    <![endif]--> 
     </div>
     <div class= "icons">
         <p id="share">Share: </p>
