6d680593e3c265a2d598bc771ae47a88c3fb1359
Cheryl Velez <onegrumpybunny@gmail.com>
2019-02-20 16:40:01 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,31 @@
 /* animated background */
+function getIEVersion() {
+    var match = navigator.userAgent.match(/(?:MSIE |Trident\/.*; rv:)(\d+)/);
+    return match ? parseInt(match[1]) : undefined;
+}
+
 $(window).on("load", function() {
-    if ($(window).width() > 450) { // only show on desktop
-        var animation = lottie.loadAnimation({
-        container: document.getElementById('animBack'),
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: '/2018/data.json'
-        });
-    
-        var animation2 = lottie.loadAnimation({
-        container: document.getElementById('animBack2'),
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: '/2018/data.json'
+    if (getIEVersion()) {
+        //do nothing because it's IE
+    } else {
+        if ($(window).width() > 450) { // only show on desktop
+            var animation = lottie.loadAnimation({
+            container: document.getElementById('animBack'),
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: '/2018/data.json'
+            });
         
-    });
-    setTimeout(function() {$("#animBack2").css("top",$("#animBack").height());},800);
+            var animation2 = lottie.loadAnimation({
+            container: document.getElementById('animBack2'),
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: '/2018/data.json'
+            
+        });
+        setTimeout(function() {$("#animBack2").css("top",$("#animBack").height());},800);
+        }
     }
-    
 });
