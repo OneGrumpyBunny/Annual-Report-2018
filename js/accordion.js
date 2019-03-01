b723a0d9d7a70fab6aad4fbe3828c824bc0b182b
Cheryl Velez <onegrumpybunny@gmail.com>
2019-03-01 12:15:11 -0500

    Update accordion.js

--- js/accordion.js
+++ js/accordion.js
@@ -82,12 +82,12 @@ $(window).scroll(function () {
     $(".fixed-header").css("background","rgb(24, 37, 53)");
     var fromtop = $(window).scrollTop();
     if (navigator.userAgent.toLowerCase().match(/(ipad|iphone)/)) {
-      $("#health_background_wrap").css({"background-position-y": fromtop+"px"});
-      $("#about_background_wrap").css({"background-position-y": fromtop+"px"});
-      $("#landing_background_wrap").css({"background-position-y": fromtop+"px"});
-      $("#safe_background_wrap").css({"background-position-y": fromtop+"px"});
-      $("#thrive_background_wrap").css({"background-position-y": fromtop+"px"});
-      $("#planet_background_wrap").css({"background-position-y": fromtop+"px"});
+      $("#health_background_wrap").css({"background-position-y": "0px"});
+      $("#about_background_wrap").css({"background-position-y": "0px"});
+      $("#landing_background_wrap").css({"background-position-y": "0px"});
+      $("#safe_background_wrap").css({"background-position-y": "0px"});
+      $("#thrive_background_wrap").css({"background-position-y": "0px"});
+      $("#planet_background_wrap").css({"background-position-y": "0px"});
   } else {
       console.log("is not safari");
   }  
